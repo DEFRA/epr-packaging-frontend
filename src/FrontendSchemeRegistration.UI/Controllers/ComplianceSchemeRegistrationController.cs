2add40ebf79f2e7a7821ac690237552712d534ef
dimitri work <dim@dimitri.daras.name>
2025-11-26 11:54:05 +0000

    SMAL-116, DD. Gather data to render tiles on cso reg page.

--- src/FrontendSchemeRegistration.UI/Controllers/ComplianceSchemeRegistrationController.cs
+++ src/FrontendSchemeRegistration.UI/Controllers/ComplianceSchemeRegistrationController.cs
@@ -6,15 +6,29 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FrontendSchemeRegistration.UI.Controllers;
 
+using Application.Enums;
+using Application.Services.Interfaces;
+using Extensions;
+using Services;
+
 [Authorize(Policy = PolicyConstants.EprFileUploadPolicy)]
 [Route(PagePaths.CsoRegistration)]
-public class ComplianceSchemeRegistrationController() : Controller
+public class ComplianceSchemeRegistrationController(
+    IComplianceSchemeService complianceSchemeService,
+    IRegistrationApplicationService registrationApplicationService
+    ) : Controller
 {
     [HttpGet]
-    public Task<IActionResult> ComplianceSchemeRegistration(string nation)
+    public async Task<IActionResult> ComplianceSchemeRegistration([FromQuery]Nation nation)
     {
-        var csoRegViewModel = new ComplianceSchemeRegistrationViewModel("foo cso", nation);
+        var userData = User.GetUserData();
+        var organisation = userData.Organisations[0];
+        var complianceSchemes = await complianceSchemeService.GetOperatorComplianceSchemes(organisation.Id.Value);
+        var cso = complianceSchemes.Single(cs => cs.NationId == (int)nation);
+        var registrationApplicationPerYearViewModels = await registrationApplicationService.BuildRegistrationApplicationPerYearViewModels(HttpContext.Session, organisation);
+
+        var csoRegViewModel = new ComplianceSchemeRegistrationViewModel(cso.Name, nation.ToString(), registrationApplicationPerYearViewModels, 2026);
         
-        return Task.FromResult<IActionResult>(View(csoRegViewModel));
+        return View(csoRegViewModel);
     }
 }
