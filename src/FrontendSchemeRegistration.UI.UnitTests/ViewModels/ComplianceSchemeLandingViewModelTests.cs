266ab7e87952805bb8e62baf74280b8091c1c635
James Atherton <james.atherton@equalexperts.com>
2025-11-26 16:58:07 +0000

    Feature/smal 115 cso sp registration choose to register members email fix (#43)
    * First pass at putting tile in frontend for user redirection
    
    * SMAL-115: The feature switch works to display or not the tile, the link from the action Link does not work yet.
    
    * SMAL-115: removing unused page path
    
    * SMAL 115: adding test and moving parsing logic into ViewModel
    
    * SMAL-115: linking tiles together
    
    * SMAL-115: action link to tag helper
    
    * Moving some logic from View into Model

---
+++ src/FrontendSchemeRegistration.UI.UnitTests/ViewModels/ComplianceSchemeLandingViewModelTests.cs
@@ -0,0 +1,21 @@
+﻿namespace FrontendSchemeRegistration.UI.UnitTests.ViewModels;
+
+using FrontendSchemeRegistration.UI.ViewModels;
+
+public class ComplianceSchemeLandingViewModelTests
+{
+    [TestCase("", 0)]
+    [TestCase("2025", 2025)]
+    [TestCase("2026", 2026)]
+    [TestCase("2027", 2027)]
+    [TestCase("a2028", 0)]
+    [TestCase("jkdjkf", 0)]
+    public async Task ComplianceSchemeLandingViewModel_ConvertsYearToIntAndHandlesFailedParsings(string complianceYear, int yearAsInt)
+    {
+        var model = new ComplianceSchemeLandingViewModel
+        {
+            ComplianceYear = complianceYear
+        };
+        Assert.That(model.ComplianceYearAsInteger(complianceYear), Is.EqualTo(yearAsInt));
+    }
+}
