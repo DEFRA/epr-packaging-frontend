e93a90abf0c0d7fbb24401b2f2484127f14593c3
Daniel Ashton <daniel.ashton@dashkat.uk>
2025-12-22 11:37:20 +0000

    SMAL-201 - More sonar

--- src/FrontendSchemeRegistration.MockServer/Program.cs
+++ src/FrontendSchemeRegistration.MockServer/Program.cs
@@ -1,5 +1,7 @@
+using System.Diagnostics.CodeAnalysis;
 using FrontendSchemeRegistration.MockServer;
 
+[assembly:ExcludeFromCodeCoverage]
 Console.WriteLine("FrontendSchemeRegistration.MockServer starting on http://localhost:9091");
 
 MockApiServer.Start();
