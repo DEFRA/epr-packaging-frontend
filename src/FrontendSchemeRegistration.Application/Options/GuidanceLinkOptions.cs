2f73fc83cf9351911127ee93445bf5cd32e36494
Daniel Ashton <daniel.ashton@dashkat.uk>
2025-12-22 13:03:02 +0000

    SMAL-172 - Change guidance link reading
    Guidance links are always static so can be removed from "config". Change to readonly static values

--- src/FrontendSchemeRegistration.Application/Options/GuidanceLinkOptions.cs
+++ src/FrontendSchemeRegistration.Application/Options/GuidanceLinkOptions.cs
@@ -5,29 +5,27 @@ using System.Diagnostics.CodeAnalysis;
 [ExcludeFromCodeCoverage]
 public class GuidanceLinkOptions
 {
-    public const string ConfigSection = "GuidanceLinks";
+    public readonly string WhatPackagingDataYouNeedToCollect = "https://www.gov.uk/guidance/how-to-collect-your-packaging-data-for-extended-producer-responsibility";
 
-    public string WhatPackagingDataYouNeedToCollect { get; set; }
+    public readonly string HowToBuildCsvFileToReportYourPackagingData = "https://www.gov.uk/government/publications/packaging-data-how-to-create-your-file-for-extended-producer-responsibility/packaging-data-file-specification-for-extended-producer-responsibility";
 
-    public string HowToBuildCsvFileToReportYourPackagingData { get; set; }
+    public readonly string HowToReportOrganisationDetails = "https://www.gov.uk/government/publications/organisation-details-how-to-create-your-file-for-extended-producer-responsibility-epr-for-packaging";
+    
+    public readonly string HowToReportPackagingData = "https://www.gov.uk/government/publications/packaging-data-how-to-create-your-file-for-extended-producer-responsibility/packaging-data-file-specification-for-extended-producer-responsibility";
+    
+    public readonly string HowToBuildCsvFileToReportYourOrganisationData = "https://www.gov.uk/government/publications/organisation-details-how-to-create-your-file-for-extended-producer-responsibility-epr-for-packaging/organisation-details-file-specification-for-extended-producer-responsibility";
 
-    public string HowToReportOrganisationDetails { get; set; }
+    public readonly string ExampleCsvFile = "https://www.gov.uk/government/publications/extended-producer-responsibility-for-packaging-example-file-for-packaging-data";
 
-    public string HowToReportPackagingData { get; set; }
+    public readonly string ProducerResponsibilitiesForPackagingWaste = "https://www.gov.uk/guidance/extended-producer-responsibility-for-packaging-who-is-affected-and-what-to-do";
 
-    public string HowToBuildCsvFileToReportYourOrganisationData { get; set; }
+    public readonly string EPR_IllustrativeFeesAndCharges = "https://www.gov.uk/guidance/extended-producer-responsibility-for-packaging-recycling-obligations-and-waste-disposal-fees";
 
-    public string ExampleCsvFile { get; set; }
+    public readonly string HowToCompleteSubsidiaryFile = "https://www.gov.uk/government/publications/groups-and-subsidiaries-how-to-create-your-file-for-extended-producer-responsibility";
 
-    public string ProducerResponsibilitiesForPackagingWaste { get; set; }
+    public readonly string YouCanPaySEPA = "https://beta.sepa.scot/about-sepa/online-payments/";
 
-    public string EPR_IllustrativeFeesAndCharges { get; set; }
-
-    public string HowToCompleteSubsidiaryFile { get; set; }
-
-    public string YouCanPaySEPA { get; set; }
-
-    public string MakeChangesToYourLimitedCompany { get; set; }
-
-    public string PrnObligation { get; set; }
+    public readonly string MakeChangesToYourLimitedCompany = "https://www.gov.uk/file-changes-to-a-company-with-companies-house";
+    
+    public readonly string PrnObligation = "https://www.gov.uk/guidance/extended-producer-responsibility-for-packaging-who-is-affected-and-what-to-do#period-you-must-report-on";
 }
