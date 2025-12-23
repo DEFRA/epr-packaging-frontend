1cb87ab93358b859216fd0a92d783bda1136c4d4
Daniel Ashton <daniel.ashton@dashkat.uk>
2025-12-23 16:07:18 +0000

    SMAL-200 - Add new mock response for pay online

--- src/FrontendSchemeRegistration.MockServer/WebApi/Payment.cs
+++ src/FrontendSchemeRegistration.MockServer/WebApi/Payment.cs
@@ -12,12 +12,28 @@ public static class Payment
 {
     public static WireMockServer WithPayments(this WireMockServer server)
     {
+        // Initiate online payment (HTML redirect)
+        MapInitiatePaymentEndpoint(server, "/online-payments", "WebApi/Responses/payment/online-payments.html");
+        MapInitiatePaymentEndpoint(server, "/v1/online-payments", "WebApi/Responses/payment/online-payments.html");
+        MapInitiatePaymentEndpoint(server, "/v2/online-payments", "WebApi/Responses/payment/online-payments.html");
+
         // Compliance scheme registration fee (default + scenario switching)
         MapFeeEndpoint(server, "/compliance-scheme/registration-fee", "WebApi/Responses/payment/compliance-default.json", "WebApi/Responses/payment/compliance-latefee.json");
         MapFeeEndpoint(server, "/v1/compliance-scheme/registration-fee", "WebApi/Responses/payment/compliance-default.json", "WebApi/Responses/payment/compliance-latefee.json");
         MapFeeEndpoint(server, "/v2/compliance-scheme/registration-fee", "WebApi/Responses/payment/compliance-default.json", "WebApi/Responses/payment/compliance-latefee.json");
 
         return server;
+    }
+
+    private static void MapInitiatePaymentEndpoint(WireMockServer server, string path, string htmlFile)
+    {
+        server.Given(Request.Create()
+                .UsingPost()
+                .WithPath(path))
+            .RespondWith(Response.Create()
+                .WithStatusCode(200)
+                .WithHeader("Content-Type", "text/html; charset=utf-8")
+                .WithBodyFromFile(htmlFile));
     }
 
     private static void MapFeeEndpoint(WireMockServer server, string path, string defaultFile, string lateFeeFile)
