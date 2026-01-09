f3055560dce66be774a4eff43d6a4d290e6e8464
Daniel Ashton <daniel.ashton@dashkat.uk>
2026-01-09 15:53:34 +0000

    SMAL-178 - Update views with new journey content

--- src/FrontendSchemeRegistration.UI.UnitTests/Controllers/FileUploadPartnershipsSuccessControllerTests.cs
+++ src/FrontendSchemeRegistration.UI.UnitTests/Controllers/FileUploadPartnershipsSuccessControllerTests.cs
@@ -11,6 +11,8 @@ using FrontendSchemeRegistration.UI.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Primitives;
+using System.Security.Claims;
+using System.Text.Json;
 using Moq;
 using UI.Controllers;
 using UI.Sessions;
@@ -102,6 +104,17 @@ public class FileUploadPartnershipsSuccessControllerTests
                 PartnershipsFileName = fileName
             });
 
+        var userData = new UserData
+        {
+            Organisations = 
+            [
+                new Organisation { Name = "Org A" }
+            ]
+        };
+        var claims = new List<Claim> { new Claim(ClaimTypes.UserData, JsonSerializer.Serialize(userData)) };
+        var identity = new ClaimsIdentity(claims, "TestAuthType");
+        _systemUnderTest.ControllerContext.HttpContext.User = new ClaimsPrincipal(identity);
+
         // Act
         var result = await _systemUnderTest.Get() as ViewResult;
 
@@ -111,9 +124,54 @@ public class FileUploadPartnershipsSuccessControllerTests
         {
             FileName = fileName,
             SubmissionId = new Guid(SubmissionId),
-            RegistrationYear = DateTime.Now.Year
+            RegistrationYear = DateTime.Now.Year,
+            OrganisationName = "Org A"
         });
 
         _submissionServiceMock.Verify(x => x.GetSubmissionAsync<RegistrationSubmission>(It.IsAny<Guid>()), Times.Once);
+    }
+
+    [Test]
+    public async Task Get_SetsOrganisationName_OnViewModel_FromUserDataClaim()
+    {
+        // Arrange
+        const string fileName = "example.csv";
+        const string organisationName = "Test Organisation";
+
+        _submissionServiceMock.Setup(x => x.GetSubmissionAsync<RegistrationSubmission>(It.IsAny<Guid>()))
+            .ReturnsAsync(new RegistrationSubmission
+            {
+                PartnershipsFileName = fileName
+            });
+
+        var userData = new UserData
+        {
+            Organisations =
+            [
+                new Organisation
+                {
+                    Name = organisationName
+                }
+            ]
+        };
+
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.UserData, JsonSerializer.Serialize(userData))
+        };
+        var identity = new ClaimsIdentity(claims, "TestAuthType");
+        var principal = new ClaimsPrincipal(identity);
+        _systemUnderTest.ControllerContext.HttpContext.User = principal;
+
+        // Act
+        var result = await _systemUnderTest.Get() as ViewResult;
+
+        // Assert
+        result.Should().NotBeNull();
+        result!.ViewName.Should().Be("FileUploadPartnershipsSuccess");
+
+        var model = result.Model as FileUploadSuccessViewModel;
+        model.Should().NotBeNull();
+        model!.OrganisationName.Should().Be(organisationName);
     }
 }
