b45461a4dcb1dd4177167d6a57ed36d3989e52b3
Mike McBain <mike.mcbain@esynergy.co.uk>
2026-01-12 10:45:19 +0000

    Feat: Add DateTimeExtensions.GetComplianceYear method

---
+++ src/FrontendSchemeRegistration.Application/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace FrontendSchemeRegistration.Application.Extensions;
+
+public static class DateTimeExtensions
+{
+    /// <summary>
+    ///     Get the Compliance Year for this <see cref="System.DateTime">DateTime</see>.
+    ///     Generally, this will be the same as the actual year, but may differ within some special time windows.
+    /// </summary>
+    public static int GetComplianceYear(this DateTime date)
+    {
+        // Special case handling for Jan 2026
+        if (date is { Year: 2026, Month: 1 }) return 2025;
+
+        return date.Year;
+    }
+}
