e38ae9cf5a5eaf60af00f446e2fcbbb03cfc376a
James Atherton <james.atherton@equalexperts.com>
2026-01-12 11:54:52 +0000

    SMAL-276: adding a differentiating character for journeys. (#127)
    * SMAL-276: adding a diierentiating charchter for journeys small: S and large: L, and not available: X, X should never occur.
    
    * Fixing three tests IDE failed to run first time round.
    
    * Fixing test.

--- src/FrontendSchemeRegistration.UI.UnitTests/Services/ReferenceNumberBuilderTests.cs
+++ src/FrontendSchemeRegistration.UI.UnitTests/Services/ReferenceNumberBuilderTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace FrontendSchemeRegistration.UI.UnitTests.Services;
 
 using Application.DTOs.Submission;
+using Application.Enums;
 using Microsoft.Extensions.Time.Testing;
 using UI.Services;
 
@@ -8,17 +9,17 @@ using UI.Services;
 public class ReferenceNumberBuilderTests
 {
     [Test]
-    [TestCase(2025, 12, 31, "2025", "January", "December", "PEPROrg25P1")]
-    [TestCase(2025, 12, 31, "2026", "January", "December", "PEPROrg26P1")]
-    [TestCase(2025, 12, 31, "2027", "January", "December", "PEPROrg27P1")]
-    [TestCase(2025, 12, 31, "2025", "January", "November", "PEPROrg25P2")]
-    [TestCase(2026, 12, 31, "2026", "January", "January",  "PEPROrg26P2")]
-    [TestCase(2026, 02, 28, "2026", "January", "February", "PEPROrg26P1")]
-    [TestCase(2028, 02, 29, "2028", "January", "February", "PEPROrg28P1")]
-    [TestCase(2028, 02, 29, "2028", "January", "Feb",      "PEPROrg28P1")]
-    [TestCase(2028, 03, 01, "2028", "January", "Feb",      "PEPROrg28P2")]
-    [TestCase(2025, 10, 31, "2026", "January", "July",     "PEPROrg26P1")]
-    public void BuildReferenceNumber_DirectProducer_ReturnsReferenceNumber(int nowYear, int nowMonth, int nowDay, string subYear, string startMonth, string endMonth, string expected)
+    [TestCase(2025, 12, 31, "2025", "January", "December", "PEPROrg25P1L", "large")]
+    [TestCase(2025, 12, 31, "2026", "January", "December", "PEPROrg26P1S", "small")]
+    [TestCase(2025, 12, 31, "2027", "January", "December", "PEPROrg27P1S", "small")]
+    [TestCase(2025, 12, 31, "2025", "January", "November", "PEPROrg25P2S", "small")]
+    [TestCase(2026, 12, 31, "2026", "January", "January",  "PEPROrg26P2S", "small")]
+    [TestCase(2026, 02, 28, "2026", "January", "February", "PEPROrg26P1S", "small")]
+    [TestCase(2028, 02, 29, "2028", "January", "February", "PEPROrg28P1S", "small")]
+    [TestCase(2028, 02, 29, "2028", "January", "Feb",      "PEPROrg28P1S", "small")]
+    [TestCase(2028, 03, 01, "2028", "January", "Feb",      "PEPROrg28P2S", "bobby mcbobface")]
+    [TestCase(2025, 10, 31, "2026", "January", "July",     "PEPROrg26P1S", "small")]
+    public void BuildReferenceNumber_DirectProducer_ReturnsReferenceNumber(int nowYear, int nowMonth, int nowDay, string subYear, string startMonth, string endMonth, string expected, string journey)
     {
         var tp = new FakeTimeProvider();
         tp.SetUtcNow(new DateTimeOffset(nowYear, nowMonth, nowDay, 0, 0, 1, TimeSpan.Zero));
@@ -29,22 +30,22 @@ public class ReferenceNumberBuilderTests
             StartMonth = startMonth
         };
         
-        var refNo = ReferenceNumberBuilder.Build(sp, "Org", tp, false, 0);
+        var refNo = ReferenceNumberBuilder.Build(sp, "Org", tp, false, 0, journey);
         
         Assert.That(refNo, Is.EqualTo(expected));
     }
     
     [Test]
-    [TestCase(2025, 12, 31, "2025", "January", "December", "PEPROrg44425P1")]
-    [TestCase(2025, 12, 31, "2026", "January", "December", "PEPROrg44426P1")]
-    [TestCase(2025, 12, 31, "2027", "January", "December", "PEPROrg44427P1")]
-    [TestCase(2025, 12, 31, "2025", "January", "November", "PEPROrg44425P2")]
-    [TestCase(2026, 12, 31, "2026", "January", "January",  "PEPROrg44426P2")]
-    [TestCase(2026, 02, 28, "2026", "January", "February", "PEPROrg44426P1")]
-    [TestCase(2028, 02, 29, "2028", "January", "February", "PEPROrg44428P1")]
-    [TestCase(2028, 02, 29, "2028", "January", "Feb",      "PEPROrg44428P1")]
-    [TestCase(2028, 03, 01, "2028", "January", "Feb",      "PEPROrg44428P2")]
-    [TestCase(2025, 10, 31, "2026", "January", "July",     "PEPROrg44426P1")]
+    [TestCase(2025, 12, 31, "2025", "January", "December", "PEPROrg44425P1L")]
+    [TestCase(2025, 12, 31, "2026", "January", "December", "PEPROrg44426P1L")]
+    [TestCase(2025, 12, 31, "2027", "January", "December", "PEPROrg44427P1L")]
+    [TestCase(2025, 12, 31, "2025", "January", "November", "PEPROrg44425P2L")]
+    [TestCase(2026, 12, 31, "2026", "January", "January",  "PEPROrg44426P2L")]
+    [TestCase(2026, 02, 28, "2026", "January", "February", "PEPROrg44426P1L")]
+    [TestCase(2028, 02, 29, "2028", "January", "February", "PEPROrg44428P1L")]
+    [TestCase(2028, 02, 29, "2028", "January", "Feb",      "PEPROrg44428P1L")]
+    [TestCase(2028, 03, 01, "2028", "January", "Feb",      "PEPROrg44428P2L")]
+    [TestCase(2025, 10, 31, "2026", "January", "July",     "PEPROrg44426P1L")]
     public void BuildReferenceNumber_ForCSO_ReturnsReferenceNumber(int nowYear, int nowMonth, int nowDay, string subYear, string startMonth, string endMonth, string expected)
     {
         var isCso = true;
@@ -59,7 +60,7 @@ public class ReferenceNumberBuilderTests
             StartMonth = startMonth
         };
         
-        var refNo = ReferenceNumberBuilder.Build(sp, "Org", tp, isCso , rowNumber);
+        var refNo = ReferenceNumberBuilder.Build(sp, "Org", tp, isCso , rowNumber, RegistrationJourney.CsoLargeProducer.ToString());
         
         Assert.That(refNo, Is.EqualTo(expected));
     }
