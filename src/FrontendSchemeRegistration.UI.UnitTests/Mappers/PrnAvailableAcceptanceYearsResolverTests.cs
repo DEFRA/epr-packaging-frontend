0fd83c1864626392df8bc46e2b7cdae8a532af5d
Mike McBain <mike.mcbain@esynergy.co.uk>
2026-01-20 17:46:37 +0000

    feat: Introduce PrnAvailableAcceptanceYearsResolver

---
+++ src/FrontendSchemeRegistration.UI.UnitTests/Mappers/PrnAvailableAcceptanceYearsResolverTests.cs
@@ -0,0 +1,70 @@
+﻿namespace FrontendSchemeRegistration.UI.UnitTests.Mappers;
+
+using Application.DTOs.Prns;
+using FluentAssertions;
+using Microsoft.Extensions.Time.Testing;
+using UI.Mappers;
+
+public class PrnAvailableAcceptanceYearsResolverTests
+{
+    private FakeTimeProvider _fakeTimeProvider;
+    private PrnAvailableAcceptanceYearsResolver _sut;
+
+    [SetUp]
+    public void Setup()
+    {
+        _fakeTimeProvider = new FakeTimeProvider();
+        _sut = new PrnAvailableAcceptanceYearsResolver(_fakeTimeProvider);
+    }
+
+    [Test]
+    [TestCaseSource(nameof(ResolverCases))]
+    public void AvailableYears_maps_correctly(ResolverTestCase testCase)
+    {
+        _fakeTimeProvider.SetUtcNow(testCase.CurrentTime);
+
+        var prn = new PrnModel
+        {
+            ObligationYear = testCase.ObligationYear.ToString(),
+            DecemberWaste = testCase.IsDecemberWaste
+        };
+
+        // Act
+        var availableYears = _sut.Resolve(prn, null!, null!, null!);
+
+        // Assert
+        availableYears.Should().BeEquivalentTo(testCase.ExpectedYears);
+    }
+
+    public static IEnumerable<TestCaseData> ResolverCases =>
+    [
+        // 2025 December Waste
+        new (new ResolverTestCase (2025, true, DateTime.Parse("2026-01-01"), [2025])),
+        new (new ResolverTestCase (2025, true, DateTime.Parse("2026-01-31 23:59:59"), [2025])),
+        new (new ResolverTestCase (2025, true, DateTime.Parse("2026-02-01"), [2026])),
+        new (new ResolverTestCase (2025, true, DateTime.Parse("2026-12-31 23:59:59"), [2026])),
+
+        // After 2025 December Waste
+        new (new ResolverTestCase (2026, true, DateTime.Parse("2026-01-01"), [2026, 2027])),
+        new (new ResolverTestCase (2026, true, DateTime.Parse("2026-12-31 23:59:59"), [2026, 2027])),
+        new (new ResolverTestCase (2026, true, DateTime.Parse("2027-01-31 23:59:59"), [2026, 2027])),
+        new (new ResolverTestCase (2026, true, DateTime.Parse("2027-02-01"), [2027])),
+
+        // 2025 Non-December Waste
+        new (new ResolverTestCase (2025, false, DateTime.Parse("2026-01-01"), [2025])),
+        new (new ResolverTestCase (2025, false, DateTime.Parse("2026-01-31 23:59:59"), [2025])),
+        new (new ResolverTestCase (2025, false, DateTime.Parse("2026-02-01"), [])),
+        new (new ResolverTestCase (2026, false, DateTime.Parse("2026-01-01"), [])),
+        new (new ResolverTestCase (2026, false, DateTime.Parse("2026-02-01"), [2026])),
+
+        // After 2025 Non-December Waste
+        new (new ResolverTestCase (2026, false, DateTime.Parse("2026-01-01"), [])),
+        new (new ResolverTestCase (2026, false, DateTime.Parse("2026-12-31 23:59:59"), [2026])),
+        new (new ResolverTestCase (2026, false, DateTime.Parse("2027-01-31 23:59:59"), [])),
+        new (new ResolverTestCase (2026, false, DateTime.Parse("2027-02-01"), [])),
+        new (new ResolverTestCase (2027, false, DateTime.Parse("2027-01-31 23:59:59"), [2027])),
+        new (new ResolverTestCase (2027, false, DateTime.Parse("2027-02-01"), [2027])),
+    ];
+
+    public record ResolverTestCase(int ObligationYear, bool IsDecemberWaste, DateTime CurrentTime, int[] ExpectedYears);
+}
---
+++ src/FrontendSchemeRegistration.UI/Mappers/PrnAvailableAcceptanceYearsResolver.cs
@@ -0,0 +1,49 @@
+﻿namespace FrontendSchemeRegistration.UI.Mappers;
+
+using Application.DTOs.Prns;
+using Application.Extensions;
+using AutoMapper;
+
+public class PrnAvailableAcceptanceYearsResolver(TimeProvider timeProvider)
+    : IValueResolver<PrnModel, object, HashSet<int>>
+{
+    /// <summary>
+    ///     Resolves the set of Years that a PRN may be accepted against at the current time.
+    /// </summary>
+    /// <remarks>
+    ///     Generally, this will just resolve to a single year unless the PRN has expired - but December Waste PRNs sometimes have two.
+    /// </remarks>
+    public HashSet<int> Resolve(PrnModel source, object _, HashSet<int> __, ResolutionContext context)
+    {
+        var now = timeProvider.GetUtcNow();
+        var thisComplianceYear = now.GetComplianceYear();
+        var prnObligationYear = int.TryParse(source.ObligationYear, out var year) ? year : 0;
+
+        // Special handling for December Waste
+        if (source.DecemberWaste)
+        {
+            // Special handling for 2025 December Waste
+            if (prnObligationYear == 2025 && thisComplianceYear is 2025 or 2026)
+                return [thisComplianceYear];
+
+            // Define the time window where users should have a choice of year for December Waste PRNs.
+            var windowEnd = new DateTime(prnObligationYear + 1, 2, 1, 0, 0, 0, DateTimeKind.Utc);
+
+            // See if we're within the time window where either year can be chosen by the user.
+            if (now < windowEnd)
+                return [prnObligationYear, prnObligationYear + 1];
+
+            // Otherwise, December Waste should still be valid for the next Compliance Year.
+            if (prnObligationYear + 1 == thisComplianceYear)
+                return [thisComplianceYear];
+        }
+
+        // The basic case for most PRNs
+        // Note that December waste may match this if it was issued prior to December for some reason (shouldn't happen).
+        if (prnObligationYear == thisComplianceYear)
+            return [prnObligationYear];
+
+        // PRN has essentially expired at the current time. It no longer has any available years to be accepted against.
+        return [];
+    }
+}
