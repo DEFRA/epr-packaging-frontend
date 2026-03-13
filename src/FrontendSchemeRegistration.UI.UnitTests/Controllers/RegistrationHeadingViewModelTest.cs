7d765593c4e2ee54aff080dd5645ca22e8c4b8df
dimitri work <dim@dimitri.daras.name>
2026-03-13 10:41:51 +0000

    SUB-155, DD. Renamed ComplianceSchemeRegistrationHeading partial to RegistrationHeading because it's used in direct producer pages also.

--- src/FrontendSchemeRegistration.UI.UnitTests/Controllers/RegistrationHeadingViewModelTest.cs
+++ src/FrontendSchemeRegistration.UI.UnitTests/Controllers/RegistrationHeadingViewModelTest.cs
@@ -3,12 +3,12 @@
 using Application.Enums;
 using UI.ViewModels.Shared;
 
-public class ComplianceSchemeRegistrationHeadingViewModelTest
+public class RegistrationHeadingViewModelTest
 {
     [Test]
-    public void ComplianceSchemeRegistrationHeadingViewModel()
+    public void RegistrationHeadingViewModel()
     {
-        var viewModel = new ComplianceSchemeRegistrationHeadingViewModel(true, RegistrationJourney.CsoLargeProducer, "Heading", null, 2025);
+        var viewModel = new RegistrationHeadingViewModel(true, RegistrationJourney.CsoLargeProducer, "Heading", null, 2025);
         Assert.That(viewModel.Heading, Is.EqualTo("Heading"));
         Assert.That(viewModel.RegistrationYear, Is.EqualTo(2025));
         Assert.That(viewModel.RegistrationJourney, Is.EqualTo(RegistrationJourney.CsoLargeProducer));
--- src/FrontendSchemeRegistration.UI/ViewModels/Shared/RegistrationHeadingViewModel.cs
+++ src/FrontendSchemeRegistration.UI/ViewModels/Shared/RegistrationHeadingViewModel.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc.Localization;
 /// If the heading does not require localising (such as a company name, use the Heading argument, otherwise, use the LocalizedHeading argument (by using the Localizer)"/>
 /// ShowRegistrationCaption determines whether the title such as Small Producer 2026 registration is displayed above the main h1 heading 
 /// </summary>
-public record ComplianceSchemeRegistrationHeadingViewModel(
+public record RegistrationHeadingViewModel(
     bool ShowRegistrationCaption,
     RegistrationJourney? RegistrationJourney,
     string? Heading, 
